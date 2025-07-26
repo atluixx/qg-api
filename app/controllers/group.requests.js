e5050d29e3102275936e720f317c7d5c36bc78f6
LuizzJS <luizz.developer@gmail.com>
2025-07-26 10:03:41 +0200

    Import getUserId function for improved user ID resolution in request handling

--- app/controllers/group.requests.js
+++ app/controllers/group.requests.js
@@ -1,4 +1,5 @@
 import handleRequest from '../functions/handle.group.request.js';
+import getUserId from '../functions/get.user.id.js';
 
 const requestController = async (req, res, login, logged, group) => {
   console.log('[⚠️] POST /requests - Iniciando processamento da solicitação.');
